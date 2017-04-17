0959a0918a2e46be49b7acb1de79b0338add7c96
Jordan Perr-Sauer <jordan@jperr.com>
2017-04-16 23:04:34 -0600

    Switched timediagram to PIL.

--- timediagram.py
+++ timediagram.py
@@ -1,36 +1,38 @@
 # Jordan Perr-Sauer
 # For Independent Study, 4/16/2017
 
-import matplotlib as mpl
-mpl.use('Agg')
-import matplotlib.pyplot as plt
-import matplotlib.patches as patches
+from PIL import Image, ImageDraw
+from random import random
 from elem_algos import *
 
+IV = (1, 0, 0, 1, 1, 0, 1, 0, 1, 1, 1, 0, 0, 0, 0, 1, 1, 0)
+
+IV = tuple([1 if random() > 0.5 else 0 for j in range(1000)])
+
+IV = tuple([1 if j==499 else 0 for j in range(500)])
+
 RULE = 110
-WIDTH = 8
-TMAX = 8
+STEPS = 500
 
-CA = ElementaryCA_TransitionGraph(RULE, WIDTH)
+BOXWIDTH = 2 #pixels
 
-fig = plt.figure()
-ax = fig.add_subplot(111)
-for t in range(TMAX):
-	if t == 0:
-		State = (1, 0, 0, 1, 1, 0, 1, 0)
+
+
+
+width = len(IV)
+
+CA = ElementaryCA_TransitionGraph(RULE, width)
+
+im = Image.new("RGB", (width*BOXWIDTH, STEPS*BOXWIDTH), "white")
+draw = ImageDraw.Draw(im)
+
+State = IV
+for t in range(STEPS):
 	xoffset = 0
 	for bit in State:
-		ax.add_patch(
-    	patches.Rectangle(
-        	(xoffset, t),   # (x,y)
-        	1,          # width
-        	1,          # height
-        	edgecolor="none",
-        	facecolor="black" if bit==1 else "white"
-    		)
-		)
+		color = "#000" if bit else "#FFF"
+		draw.rectangle([xoffset*BOXWIDTH, t*BOXWIDTH, (xoffset+1)*BOXWIDTH+1, (t+1)*BOXWIDTH+1], color)
 		xoffset = xoffset + 1
 	State = CA.elem_step(State)
-ax.set_ylim([0,8])
-ax.set_xlim([0,8])
-fig.savefig('./outputs/ca.png')
+
+im.save("./outputs/ca-img.png")
